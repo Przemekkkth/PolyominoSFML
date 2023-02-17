71588356efbab65f1de7a2b969ea604dc4e80a54
Bondrusiek <przemekkth@outlook.com>
2023-02-17 08:43:28 +0100

    refactor

--- src/player.h
+++ src/player.h
@@ -1,57 +1,11 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-//#include "Utilities/command.h"
-
-#include <SFML/Window/Event.hpp>
-
-#include <map>
-
-
-class CommandQueue;
 
 class Player
 {
     public:
-        enum Action
-        {
-            MoveLeft,
-            MoveRight,
-            MoveUp,
-            MoveDown,
-            Fire,
-            LaunchMissile,
-            ActionCount
-        };
-
-        enum MissionStatus
-        {
-            MissionRunning,
-            MissionSuccess,
-            MissionFailure
-        };
-
-
-    public:
                                 Player();
 
-        void					handleEvent(const sf::Event& event, CommandQueue& commands);
-        void					handleRealtimeInput(CommandQueue& commands);
-
-        void					assignKey(Action action, sf::Keyboard::Key key);
-        sf::Keyboard::Key		getAssignedKey(Action action) const;
-
-        void 					setMissionStatus(MissionStatus status);
-        MissionStatus 			getMissionStatus() const;
-
-    private:
-        void					initializeActions();
-        static bool				isRealtimeAction(Action action);
-
-
-    private:
-//        std::map<sf::Keyboard::Key, Action>		mKeyBinding;
-  //      std::map<Action, Command>				mActionBinding;
-        MissionStatus 							mCurrentMissionStatus;
 };
 #endif // PLAYER_H
